a52e7d9fbfec8f0e431c5bc562ae28fd1e4e30cc
Zohaib Abbasi <abbasizohaib215@gmail.com>
2026-01-01 20:36:03 +0500

    Configure MySQL database and update settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import pymysql
+pymysql.install_as_MySQLdb()
 
 from pathlib import Path
 
@@ -25,12 +27,14 @@ SECRET_KEY = 'django-insecure-_l7s7f@0zdio_mb)t74b%r#aw6%sg1s%+vjt86w81%q=#68doo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.2.2', '127.0.0.1', 'localhost', '10.99.177.55']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'users',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,12 +45,23 @@ INSTALLED_APPS = [
     # Third-party
     'rest_framework',
     'corsheaders',
+    "channels",
 
     # Local apps
-    'vehicle',
-    'users',
+    'complaints',
+    'routes',
+    'detection',
+    "alerts",
 ]
 
+ASGI_APPLICATION = "backend.asgi.application"
+
+# Redis backend for channels
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
+    },
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -65,7 +80,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR.parent / "backend" / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -85,10 +100,18 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'visiontrack_db',
+        'USER': 'root',
+        'PASSWORD': 'root',  # leave empty if no password
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        #  'OPTIONS': {
+        #     'unix_socket': '/opt/local/var/run/mariadb-10.11/mysqld.sock',
+        # },
     }
 }
+
 AUTH_USER_MODEL = 'users.CustomUser'
 
 # Password validation
@@ -164,3 +187,8 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'Trackvision240@gmail.com'
 EMAIL_HOST_PASSWORD = 'effl pesz xmht dzrk'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR/'/vehicle_pictures',  # <-- your images folder
+]
