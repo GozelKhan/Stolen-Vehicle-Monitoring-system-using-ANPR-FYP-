ac5c2f15be0618d0c33f6a904468d8f363d38590
Rai-bilalsultan <48402@students.riphah.edu.pk>
2026-04-07 10:03:25 +0500

    Delete frontend/lib directory

--- frontend/lib/formatTime.js
+++
@@ -1,9 +0,0 @@
-export const formatTime = (date) => {
-    const diff = Math.floor((new Date() - new Date(date)) / 1000);
-
-    if (diff < 60) return `${diff}s ago`;
-    if (diff < 3600) return `${Math.floor(diff / 60)} min ago`;
-    if (diff < 86400) return `${Math.floor(diff / 3600)} hr ago`;
-
-    return `${Math.floor(diff / 86400)} days ago`;
-};
